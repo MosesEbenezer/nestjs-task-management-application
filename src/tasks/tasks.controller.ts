03f0d34d38630896b70b2310e696100219944a61
Moses Ebenezer <moseschimdike@gmail.com>
2021-05-05 15:13:00 +0100

    tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './models/task.model';
 import { TasksService } from './tasks.service';
 
@@ -10,4 +11,24 @@ export class TasksController {
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    this.tasksService.deleteTaskById(id);
+  }
+
+  // @Post() // general body extraction
+  // createTask(@Body() body) {
+  //   console.log(body);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './models/task.model';
+import { Task, TaskStatus } from './models/task.model';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,26 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks.filter((value) => value.id !== id);
   }
 }
